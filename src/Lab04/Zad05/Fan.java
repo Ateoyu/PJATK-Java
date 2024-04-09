8189ef36a8fcc429b53a9ceae1c20d1e971f3a51
Marcu <ateosu@proton.me>
2024-04-09 14:09:21 +0200

    Lab04/05 im too skill issued for this.

--- src/Lab04/Zad05/Fan.java
+++ src/Lab04/Zad05/Fan.java
@@ -1,15 +1,55 @@
 package Lab04.Zad05;
 
+import java.util.Scanner;
+
 public class Fan {
+
     public enum FanSpeed {
         SLOW(1),
-        MEDIUM,
-        FAST;
-    }
-     public final int speed;
+        MEDIUM(2),
+        FAST(3);
 
-    public FanSpeed(int speed) {
+        private int speedValue;
+
+        FanSpeed(int speedValue) {
+        }
+    }
+
+    private FanSpeed speed;
+    private boolean on;
+    private double radius;
+    private String color;
+
+    public void assignSpeed(FanSpeed speed) {
         this.speed = speed;
     }
-}
 
+    Fan() {
+        this.speed = FanSpeed.MEDIUM;
+        this.on = false;
+        this.radius = 5;
+        this.color = "Red";
+    }
+
+    Fan(FanSpeed speed, boolean on, double radius, String color) {
+        this.speed = speed;
+        this.on = on;
+        this.radius = radius;
+        this.color = color;
+    }
+
+    public void setSpeed() {
+        while (true) {
+            Scanner sc = new Scanner(System.in);
+            int speedInput = sc.nextInt();
+            if (speedInput > 0 && speedInput < 4) {
+                switch (speedInput) {
+                    case 1 -> assignSpeed(FanSpeed.SLOW);
+                    case 2 -> assignSpeed(FanSpeed.MEDIUM);
+                    case 3 -> assignSpeed(FanSpeed.FAST);
+                }
+                break;
+            }
+        }
+    }
+}
--- src/Lab04/Zad05/Zad05.java
+++ src/Lab04/Zad05/Zad05.java
@@ -1,4 +1,25 @@
 package Lab04.Zad05;
 
+import java.util.Scanner;
+
 public class Zad05 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+
+        System.out.println("Enter Fan speed:");
+        int choice = sc.nextInt();
+        switch (choice) {
+            case 1 -> Fan.FanSpeed.SLOW;
+        }
+        System.out.println("Enter Fan onState:");
+        boolean onState = sc.nextBoolean();
+        System.out.println("Enter Fan radius:");
+        double radius = sc.nextInt();
+        System.out.println("Enter Fan color:");
+        String color = sc.next();
+        Fan fan1 = new Fan();
+        Fan fan2 = new Fan(Fan.FanSpeed.MEDIUM, onState, radius, color);
+    }
 }
